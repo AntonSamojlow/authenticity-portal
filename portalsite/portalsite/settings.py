1a32a3dc52089aed47596f4321241701f1d3b453
AntonSamojlow <anton.samojlow@gmail.com>
2021-11-04 20:29:11 +0100

    added whitenoise

--- portalsite/portalsite/settings.py
+++ portalsite/portalsite/settings.py
@@ -51,7 +51,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
 
 ROOT_URLCONF = 'portalsite.urls'
 
