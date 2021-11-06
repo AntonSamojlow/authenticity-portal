2bcaa0d5a3f3bed0a7733c9288a2509a0d92878e
AntonSamojlow <anton.samojlow@gmail.com>
2021-11-06 16:55:01 +0100

    added session test

--- portalsite/portal/views.py
+++ portalsite/portal/views.py
@@ -1,5 +1,18 @@
+from datetime import datetime
+from django.http.request import HttpRequest
 from django.shortcuts import render
 
 
-def index(request):
-    return render(request, 'index.html')
+def index(request: HttpRequest):
+
+    date_time_format = "%d-%m-%Y, %H:%M:%S"
+
+    first_visit_time = request.session.get('first_visit_time', None)
+    if (first_visit_time is None):
+        first_visit_time = datetime.now().strftime(date_time_format)
+        request.session['first_visit_time'] = first_visit_time
+    context = {
+        'request_time': datetime.now().strftime(date_time_format),
+        'first_visit_time': first_visit_time,
+    }
+    return render(request, 'index.html', context=context)
