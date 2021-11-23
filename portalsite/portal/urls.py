67e4f49463c5e8f9abe4542a34dba8769a076535
AntonSamojlow <anton.samojlow@gmail.com>
2021-11-23 20:53:27 +0100

    added result to uploading

--- portalsite/portal/urls.py
+++ portalsite/portal/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('measurements', login_required(views.MeasurementsView.as_view()), name='measurements'),
-    path('measurement/<int:pk>', login_required(views.MeasurementDetailView.as_view()), name='measurement')
+    path('measurement/<int:pk>', login_required(views.MeasurementDetailView.as_view()), name='measurement'),
+    path('result', login_required(views.MeasurementDetailView.as_view()), name='result')
 ]
--- portalsite/portal/views.py
+++ portalsite/portal/views.py
@@ -1,11 +1,12 @@
 from datetime import datetime
-from django.http import request
 from django.http.request import HttpRequest
+from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, permission_required
 from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView, DetailView
 from django.views.generic.list import BaseListView
+from dataclasses import dataclass
 
 from portal.forms import MeasurementUploadForm
 from portal.models import MeasurementDataType, Measurement, Source
@@ -52,34 +53,66 @@ class MeasurementsView(TemplateView, BaseListView):
 
     def post(self, request, *args, **kwargs):
         form = self.form_class(self.type_choices, self.source_choices, request.POST)
-        name_does_not_exist : bool =  Measurement.objects.filter(
-           name__exact=request.POST['measurement_name']).count() == 0
-        if (form.is_valid()
-                and 'file' in request.FILES.keys()
-                and name_does_not_exist):
-            data_type = MeasurementDataType.objects.filter(id__exact=request.POST['data_type']).first()
-            source = Source.objects.filter(id__exact=request.POST['source']).first()
-            test_json = CsvParser.to_json(request.FILES['file'])
+        name_already_exists : bool =  Measurement.objects.filter(
+           name__exact=request.POST['name']).count() > 0
 
-            measurement = Measurement()
-            measurement.name =  request.POST['measurement_name']
-            measurement.json_data = test_json
-            measurement.data_type = data_type
-            measurement.time_measured = request.POST['measurement_time']
-            measurement.user_created = request.user
-            measurement.user_changed = request.user
-            measurement.source = source
 
+
+        if not form.is_valid():
+            return Result(False, "Data was not valid").render_view()
+
+        if name_already_exists:
+            return Result(False, "Name already exists",
+            "Please go back and choose a different name").render_view()
+
+        if 'file' not  in request.FILES.keys():
+            return Result(False, "No file selected",
+            "Please go back and select a file to upload").render_view()
+
+        data_type = MeasurementDataType.objects.filter(id__exact=request.POST['data_type']).first()
+        source = Source.objects.filter(id__exact=request.POST['source']).first()
+        test_json = CsvParser.to_json(request.FILES['file'])
+
+        measurement = Measurement()
+        measurement.name =  request.POST['name']
+        measurement.json_data = test_json
+        measurement.data_type = data_type
+        measurement.time_measured = request.POST['measured']
+        measurement.user_created = request.user
+        measurement.user_changed = request.user
+        measurement.source = source
+
+        try:
             Measurement.save(measurement)
+        except Exception as exc:
+            ### TODO: Replace this error by a generic one and write stacktrace only to log
+            return Result(False, "Internal problem", str(exc)).render_view()
 
-            # return HttpResponseRedirect('/success/')
-        else:
-            pass
-
-        context = BaseListView.get_context_data(self, **kwargs)
-        context["upload_form"] = form
-        return render(request, self.template_name, context)
+        return Result(True, "Data uploaded and saved", 
+         link_address= measurement.get_absolute_url(),
+         link_text= "See uploaded data").render_view()
 
 class MeasurementDetailView(DetailView):
     model = Measurement
     template_name = 'measurement.html'
+
+
+
+@dataclass
+class Result():
+    """Result of an operation"""
+    success: bool
+    message: str
+    details_formatted: str = None
+    link_address: str = None
+    link_text: str = None
+
+    def render_view(self) -> HttpResponse:
+        context = {
+            'result' : 'Success' if self.success else 'Failure',
+            'message' : self.message,
+            'details_formatted' : self.details_formatted,
+            'link_address' : self.link_address,
+            'link_text' : self.link_text,
+        }
+        return render(request=None, template_name='result.html', context=context)
